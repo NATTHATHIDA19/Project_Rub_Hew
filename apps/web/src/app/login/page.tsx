af9f2140da61525273a08a53e2b482ed4c84a675
Alphonsev <kittikanoot.s@ku.th>
2026-03-22 19:00:19 +0700

    Refactor code structure for improved readability and maintainability

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import NavbarLR from "../components/navbarLR";
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen bg-[#FFF8EC] text-[#D5C2A3]">
+      <NavbarLR />
+    </main>
+  );
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,6 @@
-import NavbarMain from "./components/navbarMain";
-import Sidebar from "./components/sidebar";
-import FilterChipsAndAdd from "./components/filterChipsAndAdd";
+
+import { redirect } from "next/dist/client/components/navigation";
 
 export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-[#FFF8EC] text-[#D5C2A3]">
-      <NavbarMain />
-
-      <div className="flex">
-        <Sidebar />
-
-        <div className="flex-1">
-          <FilterChipsAndAdd />
-
-          <section className="p-6">
-            {/* content ตรงกลาง/การ์ดโพสต์ ใส่ตรงนี้ */}
-          </section>
-        </div>
-      </div>
-    </main>
-  );
+  redirect("/login");
 }
