96b8c71bf698f639bcd88ec6b13c696a8fc5017b
korn845 <kronked7833@gmail.com>
2026-03-22 19:49:54 +0700

    Refactor code structure for improved readability and maintainability

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,7 @@
-import NavbarMain from "./components/navbarMain";
-import Sidebar from "./components/sidebar";
-import FilterChipsAndAdd from "./components/filterChipsAndAdd";
 
-export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-[#FFF8EC] text-[#D5C2A3]">
-      <NavbarMain />
+import { redirect } from "next/navigation";
 
-      <div className="flex">
-        <Sidebar />
-
-        <div className="flex-1">
-          <FilterChipsAndAdd />
-
-          <section className="p-6">
-            {/* content ตรงกลาง/การ์ดโพสต์ ใส่ตรงนี้ */}
-          </section>
-        </div>
-      </div>
-    </main>
-  );
+export default function LoginPage() {
+    redirect("/register");
 }
+
---
+++ apps/web/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import NavbarLR from "../components/navbarLR";
+
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen bg-[#FFF8EC] text-[#D5C2A3]">
+      <NavbarLR />
+    </main>
+  );
+}
